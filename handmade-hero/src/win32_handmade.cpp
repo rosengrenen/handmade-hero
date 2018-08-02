4e6defdf05dc74eabfcf42ff91621bc859612e87
Rasmus Rosengren <rsrp98@gmail.com>
2018-08-02 18:40:54 +0200

    Day 001

---
+++ handmade-hero/src/win32_handmade.cpp
@@ -0,0 +1,8 @@
+#include <Windows.h>
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+    MessageBoxA(0, "This is Handmade Hero.", "Handmade Hero", MB_OK | MB_ICONINFORMATION);
+
+    return 0;
+}
